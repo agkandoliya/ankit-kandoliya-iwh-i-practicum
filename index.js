fa26dd5b24963198b740881941199d7dcac70406
agkandoliya <ankit.kandoliya@trooinbound.com>
2026-07-07 12:17:23 +0530

    updated work

--- index.js
+++ index.js
@@ -1,71 +1,69 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = 'pat-na1-2f50bc50-726c-4dda-b515-87db3d132524';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+const private_app_token = 'pat-na1-4afbb534-3072-4eb9-b4cb-6896300a420a'
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/', async (req, res) => {
+  const booksEndpoint = 'https://api.hubspot.com/crm/v3/objects/2-64725003';
+  const headers = {
+    Authorization: `Bearer ${private_app_token}`,
+    'Content-Type': 'application/json'
+  }
+  const params = {
+    properties: 'book_title,author,genre'
+  }
+  try {
+    const response = await axios.get(booksEndpoint, { headers, params });
+    console.log('API Response:', JSON.stringify(response.data, null, 2));
+    const books = response.data.results;
+    console.log('Book data:', JSON.stringify(books, null, 2));
+    res.render('homepage', { title: 'Books | Integrating With HubSpot I Practicum', data: books });
+  } catch (error) {
+    console.error(error);
+  }
+})
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+app.get('/update-cobj', (req, res) => {
+  try {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' }); // Render the updates.pug template
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
+
+app.post('/update-cobj', async (req, res) => {
+  const booksEndpoint = 'https://api.hubspot.com/crm/v3/objects/2-64725003';
+  const headers = {
+    Authorization: `Bearer ${private_app_token}`,
+    'Content-Type': 'application/json'
+  }
+  const data = {
+    properties: {
+      book_title: req.body.book_title,
+      author: req.body.author,
+      genre: req.body.genre
     }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+  }
+  try {
+    const response = await axios.post(booksEndpoint, data, { headers });
+    console.log('API Response:', JSON.stringify(response.data, null, 2));
+    res.redirect('/'); // Redirects to home page
+  } catch (error) {
+    console.error(error);
+  }
 });
-*/
 
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log('Server running on port 3000')); 
