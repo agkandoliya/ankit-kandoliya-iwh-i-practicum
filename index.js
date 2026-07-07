23314681ea07c03487c5bbd8ee9358bc403eb2dd
Ankit Kandoliya <ankit.kandoliya@trooinbound.com>
2026-07-07 10:58:59 +0530

    Update default update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,53 +7,23 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
+const PRIVATE_APP_ACCESS = '';
 
-// ROUTE 1 - Homepage: fetch all Book custom object records and render table
-app.get('/', async (req, res) => {
-    const url = 'https://api.hubapi.com/crm/v3/objects/2-64725003?properties=book_title,author,genre&limit=100';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-    try {
-        const resp = await axios.get(url, { headers });
-        const data = resp.data.results;
-        res.render('homepage', { title: 'Books | Integrating With HubSpot I Practicum', data });
-    } catch (error) {
-        console.error(error);
-    }
-});
+// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// ROUTE 2 - GET form to create a new Book record
-app.get('/update-cobj', (req, res) => {
-    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
-});
+// * Code for Route 1 goes here
 
-// ROUTE 3 - POST form data to create a new Book CRM record
-app.post('/update-cobj', async (req, res) => {
-    const url = 'https://api.hubapi.com/crm/v3/objects/2-64725003';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-    const body = {
-        properties: {
-            book_title: req.body.book_title,
-            author: req.body.author,
-            genre: req.body.genre
-        }
-    };
-    try {
-        await axios.post(url, body, { headers });
-        res.redirect('/');
-    } catch (error) {
-        console.error(error);
-    }
-});
+// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-/**
-* * This is sample code to give you a reference for how you should structure your calls.
+// * Code for Route 2 goes here
+
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+
+// * Code for Route 3 goes here
+
+/** 
+* * This is sample code to give you a reference for how you should structure your calls. 
 
 * * App.get sample
 app.get('/contacts', async (req, res) => {
@@ -66,7 +35,7 @@ app.get('/contacts', async (req, res) => {
     try {
         const resp = await axios.get(contacts, { headers });
         const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });
+        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
     } catch (error) {
         console.error(error);
     }
@@ -87,7 +56,7 @@ app.post('/update', async (req, res) => {
         'Content-Type': 'application/json'
     };
 
-    try {
+    try { 
         await axios.patch(updateContact, update, { headers } );
         res.redirect('back');
     } catch(err) {
